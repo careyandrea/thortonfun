14920096048b65e136ed41566e7804af61e3fe5b
Andrea Carey <andreacarey@careyw.int.thorntonacademy.org>
2016-03-07 13:33:31 -0500

    Added a note. AMC.

--- thortonfun/ViewController.swift
+++ thortonfun/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         //
         print("hi")
+        print("Andrea got here")
     }
 
     override func didReceiveMemoryWarning() {
